a924e0e0aba9270fe16d51cee42ce92dfd2319db
JS-404-000 <jindate2@gmail.com>
2025-06-04 11:21:02 +0900

    commit

--- app/src/main/java/com/example/movielistui/MainActivity.java
+++ app/src/main/java/com/example/movielistui/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] titles = {"한산: 용의 눈물", "탑건: 매버릭", "비상선언", "헤어질 결심", "덤블도어의 비밀", "헌트1", "헌트2", "헌트3", "헌트4", "헌트5"};
+
+    int[] images = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6,
+            R.drawable.movie6, R.drawable.movie6, R.drawable.movie6, R.drawable.movie6,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        CustomAdapter adapter = new CustomAdapter(titles, images);
+        recyclerView.setAdapter(adapter);
     }
 }
